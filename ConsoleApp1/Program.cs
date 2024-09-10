c693145a56b591c3900fe29fb4db070dc43f03ee
Axel Andersson <axel.andersson@elev.ga.ntig.se>
2024-09-10 11:13:48 +0200

    ghj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using System.Reflection.Metadata.Ecma335;
+Console.WriteLine("Welcome to Tamagotchi");
+
+
+
+
+Console.ReadLine();
---
+++ ConsoleApp1/weapon.cs
@@ -0,0 +1,43 @@
+public class tamagotchi
+{
+    public int hunger;
+    public int boredom;
+    public string words;
+    public bool isAlive;
+    public string name;
+    public Random generator;
+
+    public void Tick()
+    {
+
+    }
+    public void Feed()
+    {
+
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public void Teach()
+    {
+
+    }
+
+    public void PrintStats()
+    {
+
+    }
+
+    public void GetAlive()
+    {
+
+    }
+
+    public void ReduceBoredom()
+    {
+        
+    }
+}
